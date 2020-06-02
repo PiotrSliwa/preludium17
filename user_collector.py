aa36e303374fd469e1a3ca62388276a403319857
PiotrSliwa <iodosite@gmail.com>
2020-06-02 19:01:51 +0200

    Mongo

--- user_collector.py
+++ user_collector.py
@@ -1,50 +1,41 @@
 import GetOldTweets3 as got
-import json
-import sys
-
-
-def save(username, tweets):
-    tweets_json = to_json(tweets)
-    with open(f'{username}.json', 'w') as f:
-        f.write(tweets_json)
+from pymongo import MongoClient
 
 
 class TweetCollection:
-    def __init__(self, username):
+    def __init__(self, username, mongo_url, mongo_database):
         self.username = username
-        self.tweets = {}
+        self.processed_tweet_ids = set()
+        self.client = MongoClient(mongo_url)
+        self.db = self.client[mongo_database]
+        self.db_collection = self.db.tweets
+
+    def save(self, tweet):
+        doc = {**tweet, '_id': tweet['id']}
+        self.db_collection.replace_one({'_id': doc['_id']}, doc, True)
 
     def add_all(self, tweets):
-        previous_len = len(self.tweets)
+        previous_len = len(self.processed_tweet_ids)
         for tweet in tweets:
-            self.tweets[tweet.id] = tweet.__dict__
-        current_len = len(self.tweets)
+            tweet_dict = tweet.__dict__
+            self.processed_tweet_ids.add(tweet_dict['id'])
+            self.save(tweet_dict)
+        current_len = len(self.processed_tweet_ids)
         if previous_len == current_len:
             raise Exception('No new tweets added!')
-        save(self.username, self.get_all())
-
-    def get_all(self):
-        return list(self.tweets.values())
 
 
-def collect(username):
-    tweet_collection = TweetCollection(username)
+def collect(username, mongo_url, mongo_database):
+    tweet_collection = TweetCollection(username, mongo_url, mongo_database)
     tweet_criteria = got.manager.TweetCriteria().setUsername(username)
     got.manager.TweetManager.getTweets(tweet_criteria,
                                        debug=True,
                                        bufferLength=1,
                                        receiveBuffer=tweet_collection.add_all)
-    return tweet_collection.get_all()
-
-
-def to_json(tweets):
-    return json.dumps(tweets,
-                      indent=4,
-                      sort_keys=True,
-                      default=str)
+    return tweet_collection.processed_tweet_ids
 
 
 if __name__ == '__main__':
-    username = 'PCGamesN'
-    tweets = collect(username)
-    print('Finished successfully')
+    username = 'astralisgg'
+    processed_tweet_ids = collect(username, 'mongodb://localhost:27017/', 'preludium')
+    print(f'Finished successfully. Processed {len(processed_tweet_ids)} tweets.')
